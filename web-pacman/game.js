6562094177fd4cee7be91b6e1cce61bbe21a7809
JeonJihwan <jeonjihwan72@gmail.com>
2026-01-07 23:59:34 +0900

    feat: write pacman.draw() method

--- web-pacman/game.js
+++ web-pacman/game.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("canvas");
 const canvasContext = canvas.getContext("2d");
 
-const pacmanFrames = document.getElementById("animation");
+const pacmanFrames = document.getElementById("animations");
 const ghostFrames = document.getElementById("ghosts");
 
 let createRect = (x, y, width, height, color) => {
--- web-pacman/pacman.js
+++ web-pacman/pacman.js
@@ -22,7 +22,7 @@ class Pacman {
         }
     }
 
-    eat() {}
+    eat() { }
 
     moveBackwards() {
         switch (this.direction) {
@@ -71,15 +71,41 @@ class Pacman {
         return false;
     }
 
-    checkGhostCollision() {}
+    checkGhostCollision() { }
 
-    changeDirectionIfPossible() {}
+    changeDirectionIfPossible() { }
 
     changeAnimation() {
         this.currentFrame == this.frameCount ? 1 : this.currentFrame + 1;
     }
 
-    draw() {}
+    draw() {
+        canvasContext.save();
+        canvasContext.translate(
+            this.x + oneBlockSize / 2,
+            this.y + oneBlockSize / 2
+        );
+
+        canvasContext.rotate(this.direction * 90 * Math.PI / 180);
+        canvasContext.translate(
+            -this.x - oneBlockSize / 2,
+            -this.y - oneBlockSize / 2
+        );
+
+        canvasContext.drawImage(
+            pacmanFrames,
+            (this.currentFrame -1)/ oneBlockSize,
+            0,
+            oneBlockSize,
+            oneBlockSize,
+            this.x,
+            this.y,
+            this.width,
+            this.height
+        );
+
+        canvasContext.restore();
+    }
 
     getMapX() {
         return parseInt(this.x / oneBlockSize);
