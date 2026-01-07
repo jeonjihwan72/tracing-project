17ced078ece3be5aec19e7166c8b992215e13f9c
JeonJihwan <jeonjihwan72@gmail.com>
2026-01-08 00:33:02 +0900

    feat: add detail of movement and new food drawing
    next step: chapter "pacman.eat()"

--- web-pacman/game.js
+++ web-pacman/game.js
@@ -16,6 +16,7 @@ let wallColor = "#342DCA";
 let wallSpaceWidth = oneBlockSize / 1.5;
 let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
 let wallInnerColor = "black";
+let foodColor = "#FEB897";
 
 const DIRECTION_RIGHT = 4;
 const DIRECTION_UP = 3;
@@ -63,8 +64,25 @@ let update = () => {
 let draw = () => {
     createRect(0, 0, canvas.width, canvas.height, "black");
     drawWalls();
+    drawFoods();
     pacman.draw();
 };
+
+let drawFoods = () => {
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < map[0].length; j++ ) {
+            if (map[i][j] == 2) {
+                createRect(
+                    j *oneBlockSize + oneBlockSize / 3,
+                    i*oneBlockSize + oneBlockSize/3,
+                    oneBlockSize/3,
+                    oneBlockSize/3,
+                    foodColor
+                );
+            }
+        }
+    }
+}
 
 let gameInterval = setInterval(gameLoop, 1000 / fps);
 
